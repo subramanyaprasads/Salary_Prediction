11978de657b3946bf039e6dedb18500b48e32cea
subramanyaprasads <subramanyaprasads@gmail.com>
2019-12-19 19:05:25 +0530

    Update app.py
    Template folder updated in _name_

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 model = pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
-    return render_template('/templates/index.html')
+    return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
